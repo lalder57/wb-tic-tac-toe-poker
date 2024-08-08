cc5dd065cc7b798b602c5c96f2fe8bae65894e3b
Lauryn Thayne <laurynthayne@Daren-Thayne-J62060W5VX.local>
2024-08-07 23:03:23 -0700

    target cards

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+const idInput = document.querySelector("#idInput");
+const colorInput = document.querySelector("#colorInput");
+
+console.log(idInput);
+console.log(colorInput);
+
+function setCard() {
+const card = document.querySelector(`#${idInput.value}`);
+  console.log(card);
+}
+
+document.querySelector("#editCardButton").addEventListener("click", setCard);
